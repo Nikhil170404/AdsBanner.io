ca88f772bd9e54cd6595635ae558e22c7b43ad83
Nikhil170404 <prashants1704@gmail.com>
2024-08-01 01:39:45 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  output: 'export',
+  images: {
+    unoptimized: true, // Disable built-in image optimization if you use external image hosting
+  },
+};
--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-// next.config.mjs
-export default {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-  };
-  
